ceb1517218b5b43d0fad5a997022f6f23ccbf8ae
joewxlker <joewxlk3r@gmail.com>
2025-05-14 16:42:41 +0800

    💄 add logo

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 import Link from "next/link"
 import { FC } from "react"
 
@@ -6,8 +7,8 @@ export const Header: FC = () => {
     <header className="w-full border-b-container px-5">
       <a href="#main-content" className="sr-only focus:not-sr-only">Skip to main content</a>
       <div className="flex flex-row justify-between items-center py-2 max-w-[2000px] m-auto">
-        <Link href="/" className="text-3xl font-heading text-gradient">
-          J.W
+        <Link href="/">
+          <Image src="/logo.png" alt="" height={25} width={50}/>
         </Link>
         <Link href="/#contact" className="button-primary glow">
           Contact Me
